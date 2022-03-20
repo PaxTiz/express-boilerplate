52e2bf3798b8e56b2ef41ceda13346ea82205922
Valentin Cernuta <vcernuta3@gmail.com>
2022-03-20 22:45:24 +0100

    Fix import

--- src/middlewares/middleware.ts
+++ src/middlewares/middleware.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express'
 import { query, validationResult } from 'express-validator'
 import { Unauthenticated } from '../controllers/controller'
 import userRepository from '../repositories/user_repository'
-import Utils from '../utils/cryptoo'
+import Utils from '../utils/crypto'
 
 declare global {
     // eslint-disable-next-line @typescript-eslint/no-namespace
